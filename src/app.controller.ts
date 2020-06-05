50b69aba358f669d486f764d8b12fd82ffaa8bc3
Rafael Heredia Bastidas <heredia.developer@gmail.com>
2020-06-04 21:13:30 -0400

    exploring nestJs controller feature @Get @Post @HttpCode @Body @Req @Res @Param @Query @Inject @Controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,33 @@
-import { Controller, Get, Inject, Param } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Inject, Param, Post, Query, Req, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Response } from 'express';
 
 @Controller('/v1')
 class AppController {
 
   constructor(
-    @Inject("KEY1") private readonly appService: AppService) {
+    @Inject('KEY1') private readonly appService: AppService) {
   }
 
-  @Get('/hello/:id')
-  getHello(@Param('id') id): string {
+  @Get('/hello/:id/params')
+  getHello(@Param('id') id, @Query('name') name, @Req() request): string {
     console.log('id: ' + id);
+    console.log('test: ' + name);
+    console.log(request);
     return this.appService.getHello();
+  }
+
+  @Post('/hello')
+  @HttpCode(HttpStatus.CREATED)
+  postHello(@Body() payload: any): string {
+    console.log(payload);
+    return 'ok';
+  }
+
+  @Get('/names')
+  getNames(@Res() response: Response): void {
+    response.status(200);
+    response.json([{ name: 'name1' }, { name: 'name2' }]);
   }
 }
 
